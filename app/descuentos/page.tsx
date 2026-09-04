011f49832f0ba1b0620a85de78adc82f548f5b16
propublicpublicitaria-gif <propublicpublicitaria@gmail.com>
2026-09-04 18:14:08 -0300

    feat: add discounts module

---
+++ app/descuentos/page.tsx
@@ -0,0 +1,1 @@
+import Shell from "@/components/Shell";import ProPublicApp from "@/components/ProPublicApp";export default function Page(){return <Shell title="Descuentos"><ProPublicApp module="descuentos"/></Shell>}
