c979ffd371d4bf847f941c35dfb247306bee613f
propublicpublicitaria-gif <propublicpublicitaria@gmail.com>
2026-09-04 18:14:19 -0300

    feat: add user management module

---
+++ app/usuarios/page.tsx
@@ -0,0 +1,1 @@
+import Shell from "@/components/Shell";import ProPublicApp from "@/components/ProPublicApp";export default function Page(){return <Shell title="Usuarios"><ProPublicApp module="usuarios"/></Shell>}
