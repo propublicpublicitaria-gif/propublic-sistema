1dae041b31aa7a13c6683f7f3f0eb0cd83e63736
propublicpublicitaria-gif <propublicpublicitaria@gmail.com>
2026-09-04 18:14:12 -0300

    feat: add deliveries module

---
+++ app/entregas/page.tsx
@@ -0,0 +1,1 @@
+import Shell from "@/components/Shell";import ProPublicApp from "@/components/ProPublicApp";export default function Page(){return <Shell title="Entregas"><ProPublicApp module="entregas"/></Shell>}
